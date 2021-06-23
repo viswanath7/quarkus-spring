7a1eaed13955c1d763573bbf2e5420e713d7d450
viswanath <viswanath.jayachandran@persgroep.net>
2021-06-23 07:11:00 +0200

    Initial commit

---
+++ src/main/java/com/example/configuration/ApplicationConfiguration.java
@@ -0,0 +1,7 @@
+package com.example.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {}
---
+++ src/test/java/com/example/controller/GreetingControllerIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+//@NativeImageTest
+public class GreetingControllerIntegrationTest //extends GreetingControllerTest
+{
+    // Run the test using a native image rather than in JVM
+    // Path to native executable is set using "native.image.path"
+}
